6a92b67c4a447c09c2c53ac39ad5d28b11c89ea9
brunohenridson-dev <brunohenridson@gmail.com>
2026-06-22 19:54:57 -0300

    Create javascript3.js

---
+++ javascript3.js
@@ -0,0 +1,12 @@
+document.getElementById("idioma");
+mpoSelect.addEventListener('change', function() {
+const valorSelecionado = campoSelect.value;
+if(valorselecionado === english)
+    {
+    alert('you chose english,hello')
+}
+else if(valorSelecionado === français)
+    alert('vous avez choisi le français´français ,bonjour')
+}
+else if(valorselecionado === português)
+    alert('voce selecionou português,oi')
